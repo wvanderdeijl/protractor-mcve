a359c449e5539fd3c4d9cf263bcf722aa1be6716
Nick Tomlin <nick.tomlin@gmail.com>
2017-05-14 21:50:39 -0500

    Example using GH pages

--- example_spec.js
+++ example_spec.js
@@ -1,9 +1,10 @@
 
 describe('protractor with angular', function() {
   it('should allow a test to dismiss an alert', function() {
-    browser.get('http://localhost:8080')
+    browser.get('http://nick-tomlin.com/protractor-minimal-working-example/index.html')
 
     browser.driver.get('http://angularjs.org')
+    // comment out this line to trigger the unexpected alert dialog
     browser.switchTo().alert().accept()
 
     element(by.model('yourName')).sendKeys('Julie');
