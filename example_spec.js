fb17a3f9bf57099452fcf88303789f19a0b069aa
Nick Tomlin <nick.tomlin@gmail.com>
2017-05-16 09:11:57 -0500

    chore: Update example spec to be more generic

--- example_spec.js
+++ example_spec.js
@@ -1,14 +1,11 @@
-
-describe('protractor with angular', function() {
-  it('should allow a test to dismiss an alert', function() {
-    browser.get('http://nick-tomlin.com/protractor-minimal-working-example/index.html')
-
-    browser.driver.get('http://angularjs.org')
-    // comment out this line to trigger the unexpected alert dialog
-    browser.switchTo().alert().accept()
+describe('protractor', function() {
+  it('<description of your problem>', function() {
+    browser.get('http://www.angularjs.org');
 
     element(by.model('yourName')).sendKeys('Julie');
+
     var greeting = element(by.binding('yourName'));
+
     expect(greeting.getText()).toEqual('Hello Julie!');
   });
 });
